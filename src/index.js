fd0f3f55b9e0c3f4f78052e172bb07014dba9eba
zerradi <dasileker@gmail.com>
2021-03-05 12:13:54 +0100

    fix button onclick

--- src/index.js
+++ src/index.js
@@ -29,13 +29,48 @@ cleaning.list.push(room2);
 
 console.log({ projects });
 
-const form = document.querySelector('#task-form');
 
-const forminput = () => {
-  // console.log(form);
-  console.log('Inside the function!!');
-  return false;
+
+const forminput = (event) => {
+  const form = document.querySelector('#task-form');
+  const title = document.querySelector('#inputtitle').value;
+  const  date = document.querySelector('#inputdate').value;
+  const description = document.querySelector('#inputdescription').value;
+  const note = document.querySelector('#inputnote').value;
+  const priority = document.querySelector('#inputpriority').value;
+  console.log(addTask);
+
+  title.textContent(addTask);
+
+  // console.log('Inside the function!!');
+  event.preventDefault();
 // break;
 };
 
-document.querySelector('#tasksubmit').addEventListener('onclick', forminput);
+
+const addTask = (title, date, description, note, priority) => {
+
+  this.title = title;
+  this.date = date;
+  this.description = description;
+  this.note = note;
+  this.priority = priority;
+
+};
+
+addTask.title;
+addTask.date;
+addTask.description;
+addTask.note;
+addTask.priority;
+
+// console.log(addTask);
+
+
+const btn = document.querySelector('#tasksubmit');
+
+btn.onclick = forminput;
+document.querySelector('button').addEventListener('onclick', (event) => forminput);
+
+
+
