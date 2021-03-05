8ce394fbd4f4dbaa0eefe7d50efa9215fe2b5457
newhorizon-tech <alaukikinbox@gmail.com>
2021-03-05 18:45:14 +0530

    Add display todo list functionality

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const projectFactory = (name) => {
   return { name, list };
 };
 
-const defaultProject = projectFactory('Default');
-const cleaning = projectFactory('Cleaning');
-const cooking = projectFactory('Cooking');
+const defaultProject = projectFactory('default');
+const cleaning = projectFactory('cleaning');
+const cooking = projectFactory('cooking');
 
 projects.push(defaultProject);
 projects.push(cleaning);
@@ -29,33 +29,43 @@ cleaning.list.push(room2);
 
 console.log({ projects });
 
+const displayProject = (project, listElement) => {
+  listElement.innerHTML = '';
+  const listItems = project.list;
+  listItems.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.textContent = `${item.title}, Date: ${item.duedate}, Priority: ${item.priority}`;
+    listElement.appendChild(listItem);
+  });
+};
 
+// Adds a task to the project (project is a string)
+const addTaskToProject = (task, project) => {
+  const currentProject = projects.find(o => o.name === project);
+  currentProject.list.push(task);
+  const listElement = document.querySelector('#default-list');
+  displayProject(currentProject, listElement);
+};
 
-const forminput = (event) => {
+// Parses the form input
+const forminput = () => {
   const form = document.querySelector('#task-form');
   const title = document.querySelector('#inputtitle').value;
-  const  date = document.querySelector('#inputdate').value;
+  const date = document.querySelector('#inputdate').value;
   const description = document.querySelector('#inputdescription').value;
   const note = document.querySelector('#inputnote').value;
   const priority = document.querySelector('#inputpriority').value;
-  console.log(addTask);
 
-  title.textContent(addTask);
-
-  // console.log('Inside the function!!');
-  event.preventDefault();
-// break;
+  const currentTask = todoFactory(title, date, description, note, priority);
+  addTaskToProject(currentTask, 'default');
 };
 
-
 const addTask = (title, date, description, note, priority) => {
-
   this.title = title;
   this.date = date;
   this.description = description;
   this.note = note;
   this.priority = priority;
-
 };
 
 addTask.title;
@@ -66,11 +76,7 @@ addTask.priority;
 
 // console.log(addTask);
 
-
 const btn = document.querySelector('#tasksubmit');
 
 btn.onclick = forminput;
-document.querySelector('button').addEventListener('onclick', (event) => forminput);
-
-
-
+// document.querySelector('button').addEventListener('onclick', (event) => forminput);
